a4ae06bb19c22ed1885db9452deda82a6692fa5d
teaver <jerzyx11@gmail.com>
2022-03-18 23:13:51 +0100

    code from replit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log(`Hello world!`)
+// require Discord API
+const { Client, Intents } = require('discord.js');
+// require fetch API
+// const fetch = require("node-fetch");
+// changing space to space character in order to avoid spacing errors
+const sp = ("%20");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  if (msg.content === "help") {
+    msg.reply("Type \`$help\` to see help for all commands. Type \`$help command-name\` To get more information about a specific command.")
+  }
+})
+
+client.on("message", msg => {
+  if (msg.content === "$tutorial") {
+    msg.reply("Here's a brief tutorial on how to use the bot: \n Every command begins with a `$` sign. \n If you want to look up a summoner, simply type in the summoner's nickname after the `$` sign. \n Example: `$Riot Phlox`")
+  }
+})
+
+
+
+const botToken = process.env['TOKEN']
+client.login(process.env.TOKEN)
+
