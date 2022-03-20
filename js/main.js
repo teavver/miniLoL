3568f018740e884c37a9fd634fedc20fe39d1f6d
el-tumero <tumerpl@gmail.com>
2022-03-20 12:13:22 +0100

    request

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // require Discord API
-const { Client, Intents } = require('discord.js');
+import { Client, Intents } from 'discord.js';
+import request from './request';
 // require fetch API
 // const fetch = require("node-fetch");
 // changing space to space character in order to avoid spacing errors
@@ -12,23 +13,27 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 // help command w/o dollar sign
-client.on("message", msg => {
+client.on("message", async (msg) => {
   if (msg.content === "help") {
     msg.reply("Type \`$help\` to see help for all commands. Type \`$help command-name\` To get more information about a specific command.")
   }
-})
-// help command w/ dollar sign
-client.on("message", msg => {
-    if (msg.content === "$help") {
-      msg.reply("Here's a list of all commands:")
-    }
-  })
-// tutorial command
-client.on("message", msg => {
-  if (msg.content === "$tutorial") {
-    msg.reply("Here's a brief tutorial on how to use the bot: \n Every command begins with a `$` sign. \n If you want to look up a summoner, simply type in the summoner's nickname after the `$` sign. \n Example: `$Riot Phlox`")
+  if(msg.content === "user") {
+    const data = await request('eun1','l9%20itachi')
+    msg.reply(data.summonerLevel)
   }
 })
+// help command w/ dollar sign
+// client.on("message", msg => {
+//     if (msg.content === "$help") {
+//       msg.reply("Here's a list of all commands:")
+//     }
+//   })
+// tutorial command
+// client.on("message", msg => {
+//   if (msg.content === "$tutorial") {
+//     msg.reply("Here's a brief tutorial on how to use the bot: \n Every command begins with a `$` sign. \n If you want to look up a summoner, simply type in the summoner's nickname after the `$` sign. \n Example: `$Riot Phlox`")
+//   }
+// })
 
 // user input -> summoner v4 -> match v4 -> fetch data from riot API -> save it in  json 
 // read json and display in discord (5 last games and summoner info like rank, lvl, etc) -> ui interface
---
+++ js/request.js
@@ -0,0 +1,36 @@
+import fetch from "node-fetch"
+
+// const server = 'eun1'
+// const sumName = 'l9%20itachi' 
+// const sp = ("%20")
+
+
+async function request(_server, _sumName){
+    const response = await fetch(`https://${_server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${_sumName}`, {
+        method: 'get',
+        headers: {
+            "X-Riot-Token": "RGAPI-c1126e0d-c76d-47d1-82e7-5facb92a20a7"
+        }
+    })
+    const data = await response.json()
+    return {
+        accountId: data.accountId,
+        name: data.name,
+        profileIconId: data.profileIconId,
+        revisionDate: data.revisionDate,
+        summonerLevel: data.summonerLevel
+    }
+    
+
+}
+
+export default request;
+
+// async function main(){
+//     const res = await request(server, sumName)
+//     console.log(res)
+// }
+
+// main()
+
+// console.log(request())
