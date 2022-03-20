8e8b483217331fe9c002d1824357bc0fdfe02f60
el-tumero <tumerpl@gmail.com>
2022-03-20 13:03:04 +0100

    user input

--- request.js
+++ request.js
@@ -1,14 +1,11 @@
 import fetch from "node-fetch"
-
-// const server = 'eun1'
-// const sumName = 'l9%20itachi' 
-// const sp = ("%20")
+import 'dotenv/config';
 
 export default async function request(_server, _sumName){
     const response = await fetch(`https://${_server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${_sumName}`, {
         method: 'get',
         headers: {
-            "X-Riot-Token": "RGAPI-c1126e0d-c76d-47d1-82e7-5facb92a20a7"
+            "X-Riot-Token": process.env.RIOT_TOKEN
         }
     })
     const data = await response.json()
