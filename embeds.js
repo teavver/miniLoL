2d440b94060e8a662773c9af8ae7328278c23646
teaver <jerzyx11@gmail.com>
2022-03-23 21:49:06 +0100

    new tokens

--- embeds.js
+++ embeds.js
@@ -8,7 +8,8 @@ export default function endReply(data,data2,username,shortwr,serverText) {
     //    description = `summoner lvl is below 30`
     // } else if (data2.queueType == undefined && lowLvl == false) {
     // description = `summoner has no recent ranked games`
-    // } description = `dane`
+    // } else {description = `dane`
+    // }
     // 
     if (data2.queueType == undefined && data2.summonerLevel < 30) {
         description = `Summoner is below lvl 30.`
