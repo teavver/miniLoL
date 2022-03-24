5cc65e8e8789d81900b954f7c2a89bf5661e1a60
teaver <jerzyx11@gmail.com>
2022-03-24 15:47:53 +0100

    restored from rage

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import { SlashCommandBuilder } from '@discordjs/builders'
 import { REST } from '@discordjs/rest'
 import { Routes } from 'discord-api-types/v9'
-const guildId = '956465175878316032'
-const clientId = '956462698361671750'
+const guildId = '954502187164241932'
+const clientId = '954503990685626448'
 
 const commands = [
 	new SlashCommandBuilder().setName('user').setDescription('returns summoner info').addStringOption(option => option.setName('summoner').setDescription('summoner name')),
